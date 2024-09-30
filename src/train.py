f594a427b1a9851e0e0304d9aca0a0af433ee93e
Vinicius <vwcmorais@gmail.com>
2024-09-30 20:28:33 -0300

    Update local trained model

--- src/train.py
+++ src/train.py
@@ -4,6 +4,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score
 from lightgbm import LGBMClassifier
 from omegaconf import OmegaConf
+import mlflow
+
 
 # Path to train.py
 file_path = os.path.dirname(__file__)
@@ -12,31 +14,36 @@ file_path = os.path.dirname(__file__)
 conf = OmegaConf.load(os.path.join(file_path, "config.yml"))
 
 
+mlflow.set_experiment("credit-card-exp")
+
+
 def train(df, params):
-    # Get features and target name
-    features = df.columns.to_list()[1:-1]
-    target = df.columns.to_list()[-1]
+    with mlflow.start_run():
+         # Get features and target name
+        features = df.columns.to_list()[1:-1]
+        target = df.columns.to_list()[-1]
 
-    # Train test split
-    df_train, df_test = train_test_split(df, test_size=0.3, random_state=23)
+        # Train test split
+        df_train, df_test = train_test_split(df, test_size=0.3, random_state=23)
 
-    # Train model
-    clf = LGBMClassifier(**params)
-    clf.fit(df_train[features], df_train[target])
+        # Train model
+        mlflow.log_params(params)
+        clf = LGBMClassifier(**params)
+        clf.fit(df_train[features], df_train[target])
+        
+        #meta dado do modelo
+        signature = mlflow.models.infer_signature(df_train[features], clf.predict(df_train[features]))
+        mlflow.sklearn.log_model(clf, "model", signature=signature, input_example=df_train[features].iloc[:2])
 
-    # Evaluate
-    gini_train = (
-        2 * roc_auc_score(df_train[target], clf.predict_proba(df_train[features])[:, 1])
-        - 1
-    )
-    gini_test = (
-        2 * roc_auc_score(df_test[target], clf.predict_proba(df_test[features])[:, 1])
-        - 1
-    )
+        # Evaluate
+        gini_train = (2 * roc_auc_score(df_train[target], clf.predict_proba(df_train[features])[:, 1]) - 1  )
+        gini_test = ( 2 * roc_auc_score(df_test[target], clf.predict_proba(df_test[features])[:, 1]) - 1)
 
-    # Show results
-    print(f"Gini train: {gini_train:.3f}")
-    print(f"Gini test:  {gini_test:.3f}")
+        mlflow.log_metric("gini_train", gini_train)
+        mlflow.log_metric("gini_test", gini_test)
+        # Show results
+        print(f"Gini train: {gini_train:.3f}")
+        print(f"Gini test:  {gini_test:.3f}")
 
 
 def main():
